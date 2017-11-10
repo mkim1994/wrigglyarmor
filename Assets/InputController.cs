29ff3f4b36813949a0f37263c315b0bdf5b56037
mkim1994 <mkim2@andrew.cmu.edu>
2017-11-09 22:13:30 -0500

    unity project

---
+++ Assets/InputController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("/dev/cu.usbmodemHIDP1", 9600);
+	private int pValue = 0;
+
+    public int potentioMin = 0;
+	public int potentioLimit = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		stream.Open ();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = stream.ReadLine();
+        pValue = int.Parse(Regex.Match(value, @"\d+").Value);
+//		Debug.Log (pValue);
+
+
+
+    }
+
+}
